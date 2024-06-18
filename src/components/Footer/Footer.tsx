c3ae0ce4efc159ccd3e551f32a80d0a68bdcaa4b
R3MODAS <remodas7774@gmail.com>
2024-06-18 10:16:11 +0530

    next-amazona

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <p>Copyright @ 2023 - All rights reserved by Next Amazona</p>
+    </footer>
+  );
+};
+
+export default Footer;
